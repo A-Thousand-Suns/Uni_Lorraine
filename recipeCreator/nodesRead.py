7936e4caa3f3030f91b5ad07129288ee6a598390
A-Thousand-Suns <787545023@qq.com>
2020-10-15 20:23:07 +0200

    commit

---
+++ recipeCreator/nodesRead.py
@@ -0,0 +1,21 @@
+import  json
+
+path = r'G:\corpus\layer1.json'
+with open(path, 'r') as file:
+    time = 0
+    for line in file:
+        print(line)
+        time+=1
+        if(time == 2):
+            line = line[:-2]
+            print(line)
+            print(type(line))
+            dic = json.loads(line)
+            print(type(dic))
+            ingre = dic["ingredients"]
+            print(ingre)
+            for i in ingre:
+                print(i["text"])
+                text = i["text"]
+
+            break
---
+++ recipeCreator/recipeGeneration.py
@@ -0,0 +1,5 @@
+import tensorflow as tf
+
+folatA = tf.random.uniform(shape=())
+matrixA = tf.constant([[1., 2.], [3., 4.]])
+print(matrixA.dtype)
