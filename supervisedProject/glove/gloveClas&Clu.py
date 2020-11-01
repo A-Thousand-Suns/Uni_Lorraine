44d9ccd5da9168df6780ce35d5b05e4eca6eefa9
A-Thousand-Suns <787545023@qq.com>
2020-11-01 12:19:50 +0100

    commit

---
+++ supervisedProject/glove/gloveClas&Clu.py
@@ -0,0 +1,7 @@
+from gensim.test.utils import datapath, get_tmpfile
+from gensim.models import KeyedVectors
+from gensim.scripts.glove2word2vec import glove2word2vec
+
+gloveFile = datapath(r'E:\编程文件\python\Uni Lorraine\supervisedProject\glove\gloveVectors.txt')
+tmp_file = get_tmpfile(r"E:\编程文件\python\Uni Lorraine\supervisedProject\glove\test_word2vec.txt")
+glove2word2vec(gloveFile, tmp_file)
--- supervisedProject/wmd.py
+++
@@ -1,4 +0,0 @@
-import gensim
-word2vec_model = gensim.models.KeyedVectors.load_word2vec_format('GoogleNews-vectors-negative300.bin', binary=True)
-word2vec_model.init_sims(replace=True) # normalizes vectors
-distance = word2vec_model.wmdistance("string 1".split(), "string 2".split())  # Compute WMD as normal.
